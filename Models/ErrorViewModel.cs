b766c1963b0abe885db31cfc2bc239fd7c1c5f71
IvoFer9 <2212532@my.ipleiria.pt>
2024-10-28 16:12:44 +0000

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProfessionFormApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
