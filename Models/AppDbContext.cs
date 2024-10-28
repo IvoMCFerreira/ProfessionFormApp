aafac4e0c5b3fa8810440b09e62d32a00ec29cc5
IvoFer9 <2212532@my.ipleiria.pt>
2024-10-28 20:04:11 +0000

    Acrescentei os modelos Person e Profession e mudei o appdbcontext

--- Models/AppDbContext.cs
+++ Models/AppDbContext.cs
@@ -1,12 +1,11 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using PersonProfessionApp.Models;
 
 namespace ProfessionFormApp.Models
 {
     public class AppDbContext : DbContext
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
-        {
-        }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
         public DbSet<Person> People { get; set; }
         public DbSet<Profession> Professions { get; set; }
---
+++ Models/Person.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PersonProfessionApp.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public int? ProfessionId { get; set; }
+
+        [ForeignKey("ProfessionId")]
+        public Profession Profession { get; set; }
+    }
+}
