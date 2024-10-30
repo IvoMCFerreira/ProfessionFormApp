dc82053f5f192acde12d3597f003b41a4917625f
IvoFer9 <2212532@my.ipleiria.pt>
2024-10-30 19:36:00 +0000

    adicionada validaçao na criaçao/ediçao de pessoas

--- Models/Person.cs
+++ Models/Person.cs
@@ -8,8 +8,10 @@ namespace PersonProfessionApp.Models
         public int Id { get; set; }
 
         [Required]
+        [StringLength(50)]
         public string Name { get; set; } = string.Empty;
 
+        [Range(18, int.MaxValue, ErrorMessage = "Age must be 18 or older.")]
         public int Age { get; set; }
 
         public int? ProfessionId { get; set; }
