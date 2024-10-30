12ddd525fee2c8b6e52df46b7c8099f4577600da
IvoFer9 <2212532@my.ipleiria.pt>
2024-10-30 19:37:08 +0000

    adicionada validaçao profession

--- Models/Profession.cs
+++ Models/Profession.cs
@@ -7,6 +7,7 @@ namespace PersonProfessionApp.Models
         public int Id { get; set; }
 
         [Required]
+        [StringLength(50)]
         public string Name { get; set; } = string.Empty;
 
         public List<Person> People { get; set; } = new List<Person>();
